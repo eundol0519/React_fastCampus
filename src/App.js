5d460cfccab463dfe1f405479c12badcc7c84e29
오은희 <72530436+eundol0519@users.noreply.github.com>
2022-09-29 15:00:45 +0900

    React 공식문서_Props

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Compoisition from "./components/2-4.Props/Compoisition";
+import Extaction from "./components/2-4.Props/Extraction/Extaction";
+
+function App() {
+  return (
+    <div className="App">
+      <Compoisition />
+      <Extaction />
+    </div>
+  );
+}
+
+export default App;
