3006c9692668edf644be055f9acdb2f51065abf9
오은희 <72530436+eundol0519@users.noreply.github.com>
2022-09-29 15:49:13 +0900

    React 공식문서_State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Compoisition from "./components/2-4.Props/Compoisition";
 import Extaction from "./components/2-4.Props/Extraction/Extaction";
+import ClassComponent from "./components/2-5.State/ClassComponent";
+import FunctionalComponent from "./components/2-5.State/FunctionalComponent";
 
 function App() {
   return (
     <div className="App">
-      <Compoisition />
+      <ClassComponent />
+      <FunctionalComponent />
       <Extaction />
+      <Compoisition />
     </div>
   );
 }
---
+++ src/components/2-5.State/ClassComponent.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  // 컴포넌트가 그려지자마자 호출되는 함수
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  // 컴포넌트가 사라지기 직전에 호출되는 함수
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
