9eb0d2406c48faae6b1f66b2569db68efe736f90
오은희 <72530436+eundol0519@users.noreply.github.com>
2022-09-29 17:02:40 +0900

    React 공식문서_Event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Compoisition from "./components/2-4.Props/Compoisition";
-import Extaction from "./components/2-4.Props/Extraction/Extaction";
-import ClassComponent from "./components/2-5.State/ClassComponent";
-import FunctionalComponent from "./components/2-5.State/FunctionalComponent";
-import ClassComponent2 from "./components/2-6.LifeCycle/ClassComponent";
+import Event from "./components/2-7.Event/Event";
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent />
-      <FunctionalComponent />
-      <Extaction />
-      <Compoisition />
-      <ClassComponent2 />
+      <Event />
     </div>
   );
 }
